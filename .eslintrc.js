ea219833497b2a72d5a9b96237536aa46ee0978f
Jon Arve Ovesen <jonovesen@gmail.com>
2025-07-20 12:26:12 +0200

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+// .eslintrc.js
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+  },
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+};
